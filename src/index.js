c1960486301479501f96679911900c95a170a3b7
wineshuga <uzochukwuwinnie@gmail.com>
2023-07-11 23:39:05 +0100

    Add drag an drop functionality

--- src/index.js
+++ src/index.js
@@ -30,4 +30,38 @@ const showTasks = () => {
   });
 };
 
+// Drag and Drop functionality
+let draggedTask = null;
+
+listContainer.addEventListener('dragstart', (event) => {
+  draggedTask = event.target;
+});
+
+listContainer.addEventListener('dragover', (event) => {
+  event.preventDefault();
+});
+
+const updateIndices = () => {
+  const items = Array.from(listContainer.children);
+  items.forEach((item, index) => {
+    item.dataset.index = index;
+  });
+};
+
+listContainer.addEventListener('drop', (event) => {
+  event.preventDefault();
+  if (draggedTask) {
+    const targetItem = event.target;
+    const targetIndex = Number(targetItem.dataset.index);
+    const draggedIndex = Number(draggedTask.dataset.index);
+
+    if (targetIndex !== draggedIndex) {
+      const items = Array.from(listContainer.children);
+      const targetOffset = targetIndex < draggedIndex ? 0 : 1;
+      listContainer.insertBefore(draggedTask, items[targetIndex + targetOffset]);
+      updateIndices();
+    }
+  }
+});
+
 showTasks();
